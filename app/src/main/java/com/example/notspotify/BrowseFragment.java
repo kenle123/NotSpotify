4423fbe87728af2af953c10b9d08c01b46e90860
Ken Le <kle9597@gmail.com>
2019-02-05 23:48:36 -0800

    Added in username to profile page with signed in user's username

--- app/src/main/java/com/example/notspotify/BrowseFragment.java
+++ app/src/main/java/com/example/notspotify/BrowseFragment.java
@@ -4,6 +4,7 @@ package com.example.notspotify;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -123,5 +124,4 @@ public class BrowseFragment extends Fragment {
     public static String getSongID() {
         return songID;
     }
-
 }
