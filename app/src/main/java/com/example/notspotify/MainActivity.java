3c2febb03630d4d0657d577a1c23b29efa5b924c
prestonwong7 <prestonwong7@gmail.com>
2019-02-05 20:17:24 -0800

    Made two new methods you can call to find user's username and password

--- app/src/main/java/com/example/notspotify/MainActivity.java
+++ app/src/main/java/com/example/notspotify/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, BottomNavActivity.class);
             startActivity(intent);
         }
-
+//
         loginButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
