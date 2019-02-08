d22f3cb7f50f2cfe1c692bd7cf3ce3030f15bf61
Ken Le <kle9597@gmail.com>
2019-02-07 16:07:44 -0800

    Added playlist name to be displayed in library fragment

--- app/src/main/java/com/example/notspotify/LibraryFragment.java
+++ app/src/main/java/com/example/notspotify/LibraryFragment.java
@@ -33,15 +33,22 @@ public class LibraryFragment extends Fragment {
     ListView listView;
     TextView mPlaylistUser;
 
+    Session session;
+    String username;
+    UserPlaylist usersPlaylist;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_library, container, false);
 
+        // Get username from session
+        session = new Session(getActivity());
+        username = session.getUsername();
+
         // MusicList variable to hold the music information
         final PlaylistHandler playlistHandler = loadJsonIntoPlaylist();
-
 
         Session session = new Session(getActivity());
         mPlaylistUser = view.findViewById(R.id.textview_playlists);
@@ -51,13 +58,24 @@ public class LibraryFragment extends Fragment {
         final List<PlaylistSearchModel> playlist = new ArrayList<>();
         List<UserPlaylist> playlist2 = playlistHandler.getList();
 
-        //for(int i = 0; i < playlist2.size(); i++) {
-          //  playlist.add(new PlaylistSearchModel(playlist2.get(i).getUsername(), playlist2.get(i).));
-        //}
 
+        // Find which playlists belong to which user
+        for (int i = 0; i < playlist2.size(); i++) {
+            if (username.equals(playlist2.get(i).getUsername())) {
+                usersPlaylist = playlist2.get(i);
+            }
+        }
+
+        // Add playlist names to listview which will display each playlist name
+        for (int i = 0; i < usersPlaylist.getPlaylist().size(); i++) {
+            playlist.add(new PlaylistSearchModel(usersPlaylist.getPlaylist().get(i).getPlaylistName()));
+        }
+
+        // Array adapter needed to display the listview
         ArrayAdapter arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, playlist);
         listView.setAdapter(arrayAdapter);
 
+        // On click listener for when user clicks on an item in the listview
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -69,23 +87,22 @@ public class LibraryFragment extends Fragment {
 
     /**
      * Loads the users from the playlists.json file into playlist object using GSON
+     *
      * @return the populated playlists
      */
-    public PlaylistHandler loadJsonIntoPlaylist()
-    {
-        try
-        {
+    public PlaylistHandler loadJsonIntoPlaylist() {
+        try {
             String myJson = inputStreamToString(getActivity().getAssets().open("playlists.json"));
-            PlaylistHandler playlist  = new Gson().fromJson(myJson, PlaylistHandler.class);
+            PlaylistHandler playlist = new Gson().fromJson(myJson, PlaylistHandler.class);
             return playlist;
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             return null;
         }
     }
 
     /**
      * Reads a file using inputstream
+     *
      * @param inputStream a file to read from
      * @return a string of the read in file
      */
--- app/src/main/java/com/example/notspotify/PlaylistSearchModel.java
+++ app/src/main/java/com/example/notspotify/PlaylistSearchModel.java
@@ -3,26 +3,14 @@ package com.example.notspotify;
 import java.util.List;
 
 public class PlaylistSearchModel {
-    private String mUser;
     private String mPlaylistName;
-    private List<Music> mPlaylistSongs;
 
-    public PlaylistSearchModel(String user, String playlistName, List<Music> playlistSongs) {
-        this.mUser = user;
+    public PlaylistSearchModel(String playlistName) {
         this.mPlaylistName = playlistName;
-        this.mPlaylistSongs = playlistSongs;
-    }
-
-    public String getUser() {
-        return mUser;
     }
 
     public String getPlaylistName() {
         return mPlaylistName;
-    }
-
-    public List<Music> getPlaylistSongs() {
-        return mPlaylistSongs;
     }
 
     @Override
